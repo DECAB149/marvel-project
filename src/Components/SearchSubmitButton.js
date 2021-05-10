5daa8d32e5e0e76908b9439900093ef1af3c5114
Patric <paca7545@colorado.edu>
2021-05-10 04:59:40 -0700

    api works

--- src/Components/SearchSubmitButton.js
+++ src/Components/SearchSubmitButton.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import getResponse from './MarvelApiTest';
+import MD5 from 'crypto-js/md5';
+
 
 function SubmitButton (props) {
-    
+    var privateKey = "f1f3a993968d5473641680b62ecda40bc69337ef"; 
+var publicKey = '80465148c83b1065bca44bc3e6fd3e83';
+
+let baseUrl = 'https://gateway.marvel.com/v1/public/characters?';
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        getResponse();
+        fetch(marvelUrl, {mode: 'cors'})
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            console.log(response.data);
+        });
     }
     const [name, setName] = useState();
 
@@ -13,7 +25,11 @@ function SubmitButton (props) {
         setName(e.target.value);
     }
 
-  
+  var timeStamp = new Date().getTime();
+var stringtoHash = timeStamp+privateKey+publicKey;
+const hash = MD5(stringtoHash);
+var marvelUrl = baseUrl + 'name=' + name + '&ts=' + timeStamp + '&apikey=' + publicKey + '&hash=' + hash;
+console.log(marvelUrl);
 
     return (
         <form onSubmit={e => {handleSubmit(e)}}> 
