93c9ce84cd998d20703f774388952abb7734c8ee
Patric <paca7545@colorado.edu>
2021-05-10 16:13:53 -0700

    restructured api form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, } from 'react';
-import SubmitButton from './Components/SearchSubmitButton'
+import React from 'react';
+import SubmitButton from './Components/ApiCall'
 
 
 
--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,14 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MD5 from 'crypto-js/md5';
 
-
+//Component for button and call to api
 function SubmitButton (props) {
+//api keys put her for simplicity
     var privateKey = "f1f3a993968d5473641680b62ecda40bc69337ef"; 
-var publicKey = '80465148c83b1065bca44bc3e6fd3e83';
+    var publicKey = '80465148c83b1065bca44bc3e6fd3e83';
+    let baseUrl = 'https://gateway.marvel.com/v1/public/characters?';
 
-let baseUrl = 'https://gateway.marvel.com/v1/public/characters?';
+//state changes for user input
+    const [name, setName] = useState();
 
+    const handleChange = (e) => {
+        setName(e.target.value);
+    }
+//makes time stamp per Marvel Documentation and hashes data
+//also creates complete url
+    var timeStamp = new Date().getTime();
+    var stringtoHash = timeStamp+privateKey+publicKey;
+    const hash = MD5(stringtoHash);
+    var marvelUrl = baseUrl + 'name=' + name + '&ts=' + timeStamp + '&apikey=' + publicKey + '&hash=' + hash;
+    console.log(marvelUrl);
 
+//handles the api call and specific json data requested
     const handleSubmit = (e) => {
         e.preventDefault();
         fetch(marvelUrl, {mode: 'cors'})
@@ -19,18 +33,9 @@ let baseUrl = 'https://gateway.marvel.com/v1/public/characters?';
             console.log(response.data);
         });
     }
-    const [name, setName] = useState();
-
-    const handleChange = (e) => {
-        setName(e.target.value);
-    }
-
-  var timeStamp = new Date().getTime();
-var stringtoHash = timeStamp+privateKey+publicKey;
-const hash = MD5(stringtoHash);
-var marvelUrl = baseUrl + 'name=' + name + '&ts=' + timeStamp + '&apikey=' + publicKey + '&hash=' + hash;
-console.log(marvelUrl);
-
+    
+    
+//basic form with button
     return (
         <form onSubmit={e => {handleSubmit(e)}}> 
         <label>Search Characters Here</label>
